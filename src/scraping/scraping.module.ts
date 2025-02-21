f4c3a75e2e00f76b1116e490cbf2520eb380c092
Oumayma <oumayma.atouie@gmail.com>
2025-02-21 10:04:28 +0100

    Missing dependencies

--- src/scraping/scraping.module.ts
+++ src/scraping/scraping.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ArticleService } from '../article/article.service';
+import { CommentService } from '../comment/comment.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Article } from '../article/article.entity';
+import { User } from '../user/user.entity';
+import { UserService } from '../user/user.service';
+import { Comment } from '../comment/comment.entity';
 import { ScrapingService } from './scraping.service';
 import { ScrapingController } from './scraping.controller';
 
 @Module({
-  providers: [ScrapingService],
-  controllers: [ScrapingController]
+  imports: [TypeOrmModule.forFeature([Article, User, Comment])],
+  controllers: [ScrapingController],
+  providers: [ScrapingService, ArticleService, CommentService, UserService],
 })
 export class ScrapingModule {}
